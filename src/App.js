7c8c6fe644b0aa7d75bc29e29f9db677a5fa2bdb
Gaurav <iamgaurav201@gmail.com>
2023-01-13 07:48:12 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ const App = () => {
   const addExpenseHandler = (expense) =>{
       
      setExpenses((prevExpenses) => {
+      console.log(prevExpenses);
      return [expense,...prevExpenses]; 
   });
   };
